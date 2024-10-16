b3eac1e5b0409bf536ef5ed52a26a5e107b7f57b
Dagart <dagartga@gmail.com>
2024-10-16 15:43:55 -0700

    typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="olympics_data_project",
     version="0.0.0",
-    package=["olympics_data_cleaning"],
+    packages=["olympics_data_cleaning"],
     author="Dagart Allison",
     author_email="dagartga@gmail.com",
     description="A package to scrape data and clean Olympics data from the web and Kaggle.",
