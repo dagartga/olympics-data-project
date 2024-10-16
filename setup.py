936da9653aaabf9b4d51f695956f16cea21170a8
Dagart <dagartga@gmail.com>
2024-10-16 15:43:00 -0700

    explicit naming of package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="olympics_data_project",
     version="0.0.0",
-    package=find_packages(),
+    package=["olympics_data_cleaning"],
     author="Dagart Allison",
     author_email="dagartga@gmail.com",
     description="A package to scrape data and clean Olympics data from the web and Kaggle.",
