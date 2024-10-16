69d6ec4fc4539f0cd3ff638bcb101a66b3ab7bb9
Dagart <dagartga@gmail.com>
2024-10-16 15:51:43 -0700

    change project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="olympics_data_project",
+    name="olympics_data_cleaning",
     version="0.0.0",
     packages=["olympics_data_cleaning"],
     author="Dagart Allison",
