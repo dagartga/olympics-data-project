69c6ce3e694bf1e0cc66713e343a62b87a2679a9
Dagart <dagartga@gmail.com>
2024-10-16 15:57:35 -0700

    rearrange file structure

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="olympics_data_cleaning",
+    name="olympics_data_project",
     version="0.0.0",
-    packages=["olympics_data_cleaning"],
+    packages=["olympics_data_project"],
     author="Dagart Allison",
     author_email="dagartga@gmail.com",
     description="A package to scrape data and clean Olympics data from the web and Kaggle.",
